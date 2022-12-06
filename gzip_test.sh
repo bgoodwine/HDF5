67b2357b52c1e9687a2849a4b50cc8308376722b
Bridget Goodwine <bgoodwin@nd.edu>
2022-12-06 16:12:20 -0500

    gzip test for like... copying and pasting into excell

--- gzip_test.sh
+++ gzip_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash  
 ts=$(date +%s%N)  
 gzip files/katrina_frame.mp4
-echo "single frame gzip compress:   $((($(date +%s%N) - $ts)/1000000)) ms"
+echo "$((($(date +%s%N) - $ts)/1000000))"
+#echo "single frame gzip compress:   $((($(date +%s%N) - $ts)/1000000)) ms"
 ts=$(date +%s%N)  
 gzip -d files/katrina_frame.mp4.gz
-echo "single frame gzip uncompress: $((($(date +%s%N) - $ts)/1000000)) ms"
+echo "$((($(date +%s%N) - $ts)/1000000))"
+#echo "single frame gzip uncompress: $((($(date +%s%N) - $ts)/1000000)) ms"
